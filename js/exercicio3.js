d111907265b292e50310bbac73eb05d03dbc912b
heclair <heclair@hotmail.com>
2023-05-23 20:33:04 -0300

    aula06

---
+++ js/exercicio3.js
@@ -0,0 +1,5 @@
+function exer3(){
+    entrada = document.querySelector("#entrada").value;
+    // seta o corpo da div saida
+    document.querySelector("#saida").value = entrada;
+}
---
+++ js/exercicio4.js
@@ -0,0 +1,9 @@
+
+function exer4() {
+    p = document.createElement("p");
+    texto = document.createTextNode("Ana Maria");
+    p.appendChild(texto);
+    sai = document.querySelector("#saida");
+    sai.appendChild(p);
+  }
+  
